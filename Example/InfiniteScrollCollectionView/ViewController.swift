1175dc8b58854c4bc0eebd9f4c55b0d1cb11871d
Twaha Mukammel <t.mukammel@aol.com>
2017-01-25 00:12:01 +0600

    InfiniteScrollCollectionView

--- Example/InfiniteScrollCollectionView/ViewController.swift
+++ Example/InfiniteScrollCollectionView/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
 
     @IBOutlet weak var collectionView: InfiniteScrollCollectionView!
     
+    // Model
     var dataArray: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     override func viewDidLoad() {
